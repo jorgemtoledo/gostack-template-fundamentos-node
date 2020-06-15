a7fe080506394588ccae78f34d4d947eba5ebcc5
jorgemtoledo <jorge.toledo@gmail.com>
2020-06-14 23:04:30 -0300

    Up desafio

---
+++ README.md
@@ -0,0 +1,15 @@
+<img alt="GoStack" src="https://storage.googleapis.com/golden-wind/bootcamp-gostack/header-desafios.png" />
+
+<h3 align="center">
+  05 - Fundamentos do Node.js
+</h3>
+
+<p align="center">“Que ninguém se engane, só se consegue a simplicidade através de muito trabalho.(Clarice Lispector)”</blockquote>
+
+## :rocket: Link do Desafio
+
+**Link**: **[link](https://github.com/Rocketseat/bootcamp-gostack-desafios/tree/master/desafio-fundamentos-nodejs).**
+
+---
+
+Feito por Jorge Toledo
--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,8 +6,21 @@ interface Balance {
   total: number;
 }
 
+// Criar interface DTO para create transaction
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
+
+  private balance: Balance = {
+    income: 0,
+    outcome: 0,
+    total: 0,
+  };
 
   constructor() {
     this.transactions = [];
@@ -15,14 +28,31 @@ class TransactionsRepository {
 
   public all(): Transaction[] {
     // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
     // TODO
+    return this.balance;
   }
 
-  public create(): Transaction {
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
     // TODO
+    const transaction = new Transaction({ title, value, type });
+
+    if (type === 'income') {
+      this.balance.income += value;
+    }
+    if (type === 'outcome') {
+      this.balance.outcome += value;
+    }
+
+    this.balance.total = this.balance.income - this.balance.outcome;
+
+    this.transactions.push(transaction);
+
+    return transaction;
+
   }
 }
 
